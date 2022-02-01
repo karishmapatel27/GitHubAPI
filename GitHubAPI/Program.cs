6e8fa96fe8c68070ee85e1fbc9b5cf0b40bf22ff
Karishma <karishmapatel93@hotmail.com>
2022-02-02 09:21:28 +1100

    Installed RestSharp, Newtonsoft.json, getting repo names for unauthenticated user

--- GitHubAPI/Program.cs
+++ GitHubAPI/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 
 namespace GitHubAPI
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // base url
+            string url = "https://api.github.com/users/karishmapatel27/repos";
+   
+            var client = new RestClient();
+            var request = new RestRequest(url, Method.Get);
+            var response = await client.ExecuteAsync(request);
+            var names = response.Content; // was using toSting() here 
+            JArray o = JArray.Parse(names);
+      
+
+            foreach (JObject item in o)
+            {
+                string name = item.GetValue("name").ToString();
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
         }
+
+
     }
 }
