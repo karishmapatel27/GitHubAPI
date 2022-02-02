9836249bc236a4cc88ed4952d62a2e43e5ebc18c
Karishma <karishmapatel93@hotmail.com>
2022-02-03 10:19:07 +1100

    setup appsettings and getting repo list for authenticated user

--- GitHubAPI/Program.cs
+++ GitHubAPI/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Linq;
 using RestSharp;
+using RestSharp.Authenticators;
 
 namespace GitHubAPI
 {
@@ -9,24 +13,39 @@ namespace GitHubAPI
     {
         static async Task Main(string[] args)
         {
-            // base url
-            string url = "https://api.github.com/users/karishmapatel27/repos";
-   
+            // Create a service collection for DI
+            var serviceCollection = new ServiceCollection();
+
+            //build configuration
+            IConfiguration configuration;
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string url = "https://api.github.com/user/repos?affiliation=owner";
+            var userName = configuration.GetSection("userName").Value;
+            var password = configuration.GetSection("password").Value;
+            int counter = 1;
+
             var client = new RestClient();
+            client.Authenticator = new HttpBasicAuthenticator(userName, password);
+
             var request = new RestRequest(url, Method.Get);
             var response = await client.ExecuteAsync(request);
-            var names = response.Content; // was using toSting() here 
-            JArray o = JArray.Parse(names);
-      
 
-            foreach (JObject item in o)
+            var names = response.Content;
+            JArray result = JArray.Parse(names);
+            foreach (JObject item in result)
             {
-                string name = item.GetValue("name").ToString();
-                Console.WriteLine(name);
+                string name = item.GetValue("name").
+                    ToString();
+                // OR string name = (string)item.GetValue("name");
+                Console.WriteLine(counter + "." + name);
+                counter++;
             }
-            Console.ReadLine();
+
+            Console.Read();
         }
-
-
     }
 }
